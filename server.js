b75290616f2114777c844c325cc3eab035d2ddc5
William Garcia-Sanchez <89278772+Wixx19@users.noreply.github.com>
2023-01-26 14:03:22 -0500

    a01

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm).
-const mini mist = require('minimist');
+const minimist = require('minimist');
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 let args = minimist(process.argv.slice(2));
@@ -22,19 +22,17 @@ const port = args.port || 3000;
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 fs.readFile('./public/index.html', 'utf-8', (err, data) => { if (err){console.error(err); return; }
-console.log(data);
-});
+console.log(data); });
 
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
+res.setHeader('Content-Type', `text/html`);
 res.end('./public/index/html');
 });
 
-server.listen(port, () => console.log('Server listening on port ' + port); 
+
+server.listen(port, () => {console.log('Server listening on port ' + port); 
 });
-
-
 
 
 // Define a const `server` as an arrow function using http.createServer. 
@@ -43,8 +41,6 @@ server.listen(port, () => console.log('Server listening on port ' + port);
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
-
-
 
 
 
